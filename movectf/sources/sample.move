5ba3e8f58b5f49d5cba8e68e675761b9321b28af
Shawn <shawn@karboncard.com>
2022-11-06 22:04:41 +0800

    add flag

--- movectf/sources/sample.move
+++ movectf/sources/sample.move
@@ -2,10 +2,7 @@ module ctfone::solution {
   use sui::tx_context::{TxContext};
   use game::hero::{Self, Hero};
   use game::adventure::{slay_boar, slay_boar_king};
-  use game::inventory::{Self, TreasuryBox};
-  use ctf::random;
-
-  const WRONG_RANDOM: u64 = 1;
+  use game::inventory::{TreasuryBox, get_flag};
 
   fun farmToLevel2(hero: &mut Hero, ctx: &mut TxContext) {
     while (hero::experience(hero) < 100) {
@@ -14,22 +11,14 @@ module ctfone::solution {
     hero::level_up(hero);
   }
 
-  fun farmKingTillEnd(hero: &mut Hero, ctx: &mut TxContext) {
+  public entry fun farmForBox(hero: &mut Hero, ctx: &mut TxContext) {
+    farmToLevel2(hero, ctx);
     while (hero::stamina(hero) >= 2) {
       slay_boar_king(hero, ctx);
     }
   }
 
-  public entry fun farm_box(hero: &mut Hero, ctx: &mut TxContext) {
-    let d100 = random::rand_u64_range(0, 100, ctx);        
-    assert!(d100 == 0, WRONG_RANDOM);
-    farmToLevel2(hero, ctx);
-    farmKingTillEnd(hero, ctx);
-  }
-
-  public entry fun get_flag(box: TreasuryBox, ctx: &mut TxContext) {
-    let d100 = random::rand_u64_range(0, 100, ctx);        
-    assert!(d100 == 0, WRONG_RANDOM);
-    inventory::get_flag(box, ctx);
+  public entry fun openBox(box: TreasuryBox, ctx: &mut TxContext) {
+    get_flag(box, ctx);
   }
 }
--- typescript/call_module.ts
+++ typescript/call_module.ts
@@ -1,42 +1,31 @@
 import { signer } from "./common";
-const heroId = '0x7f8d767663252ca4756fa4d7122bb5450f006f9d';
-const solutionId = '0x9fd671e2598d3b47e488386952b8e6fe14d9c2e1';
+const heroId = '0xd675d36a9cf8f16d9d46c90db3bbb63dced6afa2';
+const solutionId = '0x21b2185353a1545639e2dc6ae2a933a6d416da09';
+
+const boxId = '0x0b60d1c0aacb81dd16db7bf955023baad976f6f0';
 
 const farmBox = async () => {
   const moveCallTxn = await signer.executeMoveCallWithRequestType({
     packageObjectId: solutionId,
     module: 'solution',
-    function: 'farm_box',
+    function: 'farmForBox',
     typeArguments: [],
     arguments: [heroId],
     gasBudget: 1000000,
   });
   console.log('moveCallTxn', moveCallTxn);
 }
-const getFlag = async () => {
+const openBox = async () => {
   const moveCallTxn = await signer.executeMoveCallWithRequestType({
     packageObjectId: solutionId,
     module: 'solution',
-    function: 'get_flag',
+    function: 'openBox',
     typeArguments: [],
-    arguments: ['0x347fc1d89b11021c24b814bd9f3cd5ae489d9f63'],
+    arguments: [boxId],
     gasBudget: 1000000,
   });
   console.log('moveCallTxn', moveCallTxn);
 }
 
-(async () => {
-  let i = 0;
-  while (i <= 300) {
-    await farmBox();
-    i += 1;
-  }
-})();
-
-// (async () => {
-//   let i = 0;
-//   while (i <= 100) {
-//     await getFlag();
-//     i += 1;
-//   }
-// })();
+// farmBox();
+openBox();
